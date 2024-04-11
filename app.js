861d7fa112e8ab5e1e3d4fd054d0936871986f54
David Meza <mezadav98@yahoo.com>
2024-04-10 22:40:20 -0700

    feat: set baseline for app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const game = document.getElementById('game')
+const movement = document.getElementById('movement')
+const score = document.getElementById('score')
+const score2 = document.getElementById('score2')
+const status = document.getElementById('status')
+const ctx = game.getContext('2d');
+let player1;
+let player2;
+let meatball;
