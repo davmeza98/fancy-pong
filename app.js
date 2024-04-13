89f741d6ade33b13049b857a0fc88f959b488246
David Meza <mezadav98@yahoo.com>
2024-04-13 11:14:09 -0700

    feat: get playerPlate elements and set canvas size

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ const player1Element = document.getElementById('playername')
 const player2Element = document.getElementById('playername2')
 const status = document.getElementById('status')
 const ctx = game.getContext('2d');
+
+let plate1;
+let plate2;
+let meatball;
+
+const playerPlate1 = document.getElementById('plate1');
+const playerPlate2 = document.getElementById('plate2');
+
+console.log(playerPlate1)
+game.width = "900";
+game.height = "600";
+
