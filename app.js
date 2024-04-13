5db58658cee9135a03522aa74560ad133dfe09eb
David Meza <mezadav98@yahoo.com>
2024-04-13 10:42:46 -0700

    feat: declare base java script elements

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const game = document.getElementById('game')
 const movement = document.getElementById('movement')
-const score = document.getElementById('score')
-const score2 = document.getElementById('score2')
+const scoreElement= document.getElementById('score')
+const score2Element = document.getElementById('score2')
+const player1Element = document.getElementById('playername')
+const player2Element = document.getElementById('playername2')
 const status = document.getElementById('status')
 const ctx = game.getContext('2d');
-let player1;
-let player2;
-let meatball;
