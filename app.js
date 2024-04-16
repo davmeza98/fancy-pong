6b0abd5ceae3cfd34f2f03566b8abe4d8a582d28
David Meza <mezadav98@yahoo.com>
2024-04-15 21:00:04 -0700

    feat: add game reset function and update visual assets

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const ctx = game.getContext('2d');
 const startScreen = document.getElementById('start');
 const startButton = document.getElementById('start-button')
 const container = document.getElementById('container');
-const playerInput = document.getElementById('player')
-
+const playerInput = document.getElementById('player');
+const winScreen = document.getElementById('win');
 console.log(status)
 
 let plate1;
 let plate2;
 let meatball;
-
+let runGame;
 
 
 const playerPlateImg1 = document.getElementById('plate1');
@@ -23,21 +23,22 @@ const playerPlateImg2 = document.getElementById('plate2');
 const meatballImg = document.getElementById('meatball');
 //paint initial screen
 startButton.addEventListener('click', startGameScreen);
+
 function startGameScreen() {
     startScreen.style.opacity= '0';
     container.style.transitionDelay = '1s'
     container.style.opacity = '1';
     player1Element.textContent = playerInput.value;
-    let runGame = setInterval(gameLoop, 60);
+    runGame = setInterval(gameLoop, 60);
    }
 
 
 window.addEventListener('DOMContentLoaded', function(){
     //plate1 = new Player(785, 150, playerPlateImg1, 10, 75); not working- Follow up
     //plate2 = new Player(5, 150, playerPlateImg2, 10, 75); not working - follow up
-    plate1 = new Player(785, 150, 'silver', 10, 75)
-    plate2 = new Player(5, 150, 'silver', 10, 75)
-    meatball = new Meatball(400, 200, 'brown', 10, 10, 10);
+    plate1 = new Player(770, 150, playerPlateImg2, 40, 90)
+    plate2 = new Player(5, 150, playerPlateImg1, 40, 90)
+    meatball = new Meatball(400, 200, meatballImg, 10, 10, 10);
 });
 
 document.addEventListener('keydown', movementHandler);
@@ -49,36 +50,37 @@ game.setAttribute('width', getComputedStyle(game)['width']);
 //set player classes
 
 class Player {
-    constructor(x, y, color, width, height) {
+    constructor(x, y, image, width, height) {
         this.x = x;
         this.y = y;
-        this.color = color;
+        this.image = image
+        //this.color = color;
         this.width = width;
         this.height = height;
         
 
-       // this.render = function() {
-          //ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        //}
-        this.render = function(){
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+        this.render = function() {
+          ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+        //this.render = function(){
+            //ctx.fillStyle = this.color;
+            //ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-}
 class Meatball {
-    constructor(x, y, color, radius, vx, vy){
+    constructor(x, y, image, radius, vx, vy){
         this.x = x;
         this.y = y;
-        this.color = color;
+        this.image = image;
         this.radius = radius;
         this.vx = vx;
         this.vy = vy;
     
     this.render = function(){
-        ctx.fillStyle = this.color
+        //ctx.fillStyle = this.color
+        ctx.drawImage(this.image, this.x, this.y, 40, 40)
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2); //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Advanced_animations
+        //ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2); //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Advanced_animations
         ctx.fill();
         }
     }
@@ -125,13 +127,13 @@ function gameLoop() {
             meatball.vy = -meatball.vy;
 
     if (score === 6 || score2 === 6){
+        showWinner()
     clearInterval(runGame);
-    showWinner()
     }
 }
 }
 function detectHit(meatball, plate1){
-if (meatball.x + meatball.radius >= plate1.x - plate1.width &&
+if (meatball.x + meatball.radius >= plate1.x &&
     meatball.y + meatball.radius >= plate1.y &&
     meatball.y <= plate1.y + plate1.height){
         console.log('hit here')
@@ -142,7 +144,7 @@ if (meatball.x + meatball.radius >= plate1.x - plate1.width &&
 
 function detectHit2(meatball, plate2){
    
-    if (meatball.x - meatball.radius <= plate2.x + plate2.width &&
+    if (meatball.x - meatball.radius <= plate2.x  &&
         meatball.y + meatball.radius >= plate2.y &&
         meatball.y - meatball.radius <= plate2.y + plate2.height){
             console.log('hit here')
@@ -151,8 +153,8 @@ function detectHit2(meatball, plate2){
         }
     }
 
-    let score = 0
-    let score2 = 0
+    let score = 5   
+    let score2 = 5
 
 function meatballReset(ball){
     if (ball.x + ball.radius < 0) {
@@ -179,11 +181,26 @@ function meatballReset(ball){
 
 function showWinner(){
     if (score === 6) {
-        status.textContent = player1Element.textContent + ' wins! Not going broke today!'
+        winScreen.textContent = 'Congrats ' + player1Element.textContent + ', you won! Play again?'
     } else if (score2 === 6){
-        status.textContent = 'Harold won.....better get to washing dishes.';
-
+        winScreen.textContent = 'Looks like you just lost $5000...try again?'
     }
     startScreen.style.opacity = '1'
-    startScreen.style.transitionDelay = '5s'
+    startScreen.style.transitionDelay = '1s'
+
+    gameReset()
+
+}
+function gameReset(){
+    score = 0;
+    score2 = 0;
+    scoreElement.textContent = score
+    score2Element.textContent = score2
+
+    meatball.x = game.width / 2
+    meatball.y = game.height / 2
+    meatball.vx = -10
+    meatball.vy = -10
+
+    status.textContent = 'Who is gonna pay the tab this time?'
 }
